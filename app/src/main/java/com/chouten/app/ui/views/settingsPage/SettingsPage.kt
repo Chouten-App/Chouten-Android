def2bf0fc3734abcb931e89b33a87cc972e5b5d0
Toby Bridle <foreignspaghettibolognese@gmail.com>
2023-04-22 00:46:01 +0100

    feat: 🎉 theme clipboard export

--- app/src/main/java/com/chouten/app/ui/views/settingsPage/SettingsPage.kt
+++ app/src/main/java/com/chouten/app/ui/views/settingsPage/SettingsPage.kt
@@ -1,20 +1,49 @@
 package com.chouten.app.ui.views.settingsPage
 
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context
 import androidx.compose.material3.Text
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.CopyAll
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalContext
+import com.chouten.app.R
 import com.chouten.app.data.ChoutenSetting
 import com.chouten.app.data.Preferences
+import kotlin.reflect.full.declaredMemberProperties
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Preview
 @Composable
 fun SettingsPage(provider: SettingsPageViewModel = SettingsPageViewModel()) {
+    val context = LocalContext.current
+    val themes =
+        Pair(dynamicLightColorScheme(context), dynamicDarkColorScheme(context))
+    val exportJson = remember {
+        val getColours: ((ColorScheme) -> String) = { theme ->
+            theme::class.declaredMemberProperties.joinToString(",\n") { member ->
+                val hexColor =
+                    Integer.toHexString((member.getter.call(theme) as Color).toArgb())
+                        .drop(2)
+                "\t\t\"${member.name}\": \"#$hexColor\""
+            }
+        }
+
+        val lightPairs = getColours(themes.first)
+        val darkPairs = getColours(themes.second)
+        "{\n\t\"light\": {\n$lightPairs\n\t},\n\t\"dark\": {\n$darkPairs\n\t}\n}"
+    }
+
     Scaffold(topBar = {
         TopAppBar(title = {
             Text("Settings")
@@ -28,6 +57,25 @@ fun SettingsPage(provider: SettingsPageViewModel = SettingsPageViewModel()) {
             SettingsToggle(
                 provider, preference = Preferences.Settings.dynamicColor
             )
+            SettingsItem(modifier = Modifier
+                .clickable {
+                    val clipboardManager =
+                        context.getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+                    val clipData =
+                        ClipData.newPlainText(
+                            "Material You Theme",
+                            exportJson
+                        )
+                    clipboardManager.setPrimaryClip(clipData)
+                },
+                { },
+                { Text(text = stringResource(R.string.export_theme__title)) },
+                { Text(text = stringResource(R.string.export_theme__desc)) }) {
+                Icon(
+                    Icons.Default.CopyAll,
+                    stringResource(R.string.export_theme__desc)
+                )
+            }
         }
     }
 }
